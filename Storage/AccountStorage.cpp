3daae1757c2181ca3c0bd067a8ae6a1d98a56f4c
Ahmed <zahmedkhaled74@gmail.com>
2025-05-09 21:38:02 +0300

    add Account

---
+++ Storage/AccountStorage.cpp
@@ -0,0 +1,58 @@
+#include <iostream> 
+#include <vector>
+#include <Account.cpp>
+
+using namespace std;
+
+class AccountStorage
+{
+private:
+    vector<Account>Accounts;
+public:
+    void InsertAccount(const Account &Account)
+    {
+        Accounts.push_back(Account);
+    }
+
+    Account* GetAccountById(int AccountId)
+    {
+        for(auto &Account : Accounts)
+        {
+            if(Account.getAccountId() == AccountId)
+            {
+                return &Account;
+            }
+        }
+        return nullptr;
+    }
+
+    bool DeleteAcountId(int AccountId)
+    {
+        for(auto it = Accounts.begin() ; it != Accounts.end() ; it++ )
+        {
+            if(it->getAccountId() == AccountId)
+            {
+                Accounts.erase(it);
+                cout<<"Delete Account. \n";
+                return true;
+            }
+        }
+        cout<<"Account not found.\n";
+        return false;
+    }
+
+    void DisplayAllAccounts() const
+    {
+        if (Accounts.empty())
+        {
+            cout << "No Accounts stored.\n";
+            return;
+        }
+
+        for (auto Account : Accounts)
+        {
+            Account.printAccount();
+        }
+    }
+
+};
--- main.cpp
+++ main.cpp
@@ -3,5 +3,5 @@ using namespace std;
 
 int main() 
 {
-    cout << "Lesg";
+    cout << "Lesgo";
 }
